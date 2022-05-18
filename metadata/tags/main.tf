3fe1109058640cc546dec0ae8e763f3f9a02c19a
ella <ellartwismer@gmail.com>
2022-05-17 23:59:27 -0400

    adds provider to tags module

--- metadata/tags/main.tf
+++ metadata/tags/main.tf
@@ -1,5 +1,9 @@
 # DO tags
 # largely used for identification and firewall rules
+provider "digitalocean" {
+  token = var.do_token
+}
+
 resource "digitalocean_tag" "k8s_cluster" {
   name = "k8s_cluster"
 }

f361b0a67cc091efb6dd1cc0ce6c842c35fa0b69
ella <ellartwismer@gmail.com>
2022-05-18 00:54:12 -0400

    adds required_providers block to tags/main.tf but i dont wanna

--- metadata/tags/main.tf
+++ metadata/tags/main.tf
@@ -1,5 +1,14 @@
 # DO tags
 # largely used for identification and firewall rules
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
 resource "digitalocean_tag" "k8s_cluster" {
   name = "k8s_cluster"
 }

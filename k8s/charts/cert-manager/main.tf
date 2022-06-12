bcf6f4fe4992e17d8d470bbcfee06a4193ef0905
ella <ellartwismer@gmail.com>
2022-06-12 00:19:56 -0400

    uncomment cert-manager manifest since cert-manager was installed at this point

--- k8s/charts/cert-manager/main.tf
+++ k8s/charts/cert-manager/main.tf
@@ -13,30 +13,30 @@ resource "helm_release" "cert-manager" {
 
 }
 
-# resource "kubernetes_manifest" "clusterissuer_letsencrypt" {
-#   manifest = {
-#     "apiVersion" = "cert-manager.io/v1"
-#     "kind" = "ClusterIssuer"
-#     "metadata" = {
-#       "name" = "${var.service_name}-ci"
-#     }
-#     "spec" = {
-#       "acme" = {
-#         "email" = var.cert_email
-#         "privateKeySecretRef" = {
-#           "name" = "${var.service_name}-tls"
-#         }
-#         "server" = "https://acme-v02.api.letsencrypt.org/directory"
-#         "solvers" = [
-#           {
-#             "http01" = {
-#               "ingress" = {
-#                 "class" = "nginx"
-#               }
-#             }
-#           },
-#         ]
-#       }
-#     }
-#   }
-# }
+resource "kubernetes_manifest" "clusterissuer_letsencrypt" {
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind" = "ClusterIssuer"
+    "metadata" = {
+      "name" = "${var.service_name}-ci"
+    }
+    "spec" = {
+      "acme" = {
+        "email" = var.cert_email
+        "privateKeySecretRef" = {
+          "name" = "${var.service_name}-tls"
+        }
+        "server" = "https://acme-v02.api.letsencrypt.org/directory"
+        "solvers" = [
+          {
+            "http01" = {
+              "ingress" = {
+                "class" = "nginx"
+              }
+            }
+          },
+        ]
+      }
+    }
+  }
+}
